26b86e9060bbef9ab6c9db88ce85b473c2471667
Nuno Courela <nuno.courela@truphone.com>
2021-10-17 13:21:06 +0100

    Multiple root items array test

--- UnitTests/Arrays/OutputTests.cs
+++ UnitTests/Arrays/OutputTests.cs
@@ -72,6 +72,20 @@ namespace JUST.UnitTests.Issues
         }
 
         [Test]
+        public void MultipleArraysJoinArrays()
+        {
+            var input = "{\"Order\": {\"OrderId\": 123456,\"OrderLines\": [{\"SkuId\": 357159,\"Quantity\": 12.5},{\"SkuId\": 484186,\"Quantity\": 10}]}}";
+            var transformer = "[{ \"#loop($.Order.OrderLines)\": { \"OrderId\": \"#valueof($.Order.OrderId)\", \"SkuId\": \"#currentvalueatpath($.SkuId)\", \"Quantity\": \"#currentvalueatpath($.Quantity)\" } }, { \"#loop($.Order.OrderLines)\": { \"OrderId\": \"#valueof($.Order.OrderId)\", \"SkuId\": \"#currentvalueatpath($.SkuId)\", \"Quantity\": \"#currentvalueatpath($.Quantity)\" } }]";
+            var context = new JUSTContext
+            {
+                EvaluationMode = EvaluationMode.Strict | EvaluationMode.JoinArrays
+            };
+            var result = new JsonTransformer(context).Transform(transformer, input);
+
+            Assert.AreEqual("[{\"OrderId\":123456,\"SkuId\":357159,\"Quantity\":12.5},{\"OrderId\":123456,\"SkuId\":484186,\"Quantity\":10},{\"OrderId\":123456,\"SkuId\":357159,\"Quantity\":12.5},{\"OrderId\":123456,\"SkuId\":484186,\"Quantity\":10}]", result);
+        }
+
+        [Test]
         public void ArrayWithArrayInside()
         {
             var input = "{\"Order\": {\"OrderId\": 123456,\"OrderLines\": [{\"SkuId\": 357159,\"Quantity\": 12.5},{\"SkuId\": 484186,\"Quantity\": 10}]}}";
